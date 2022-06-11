c1c0df75c9b32904811e2ac9f24c1acfce6b22bb
Talia Palermo Sosa <palermotaliabelen@gmail.com>
2022-06-10 21:14:18 -0300

    paginaweb2

---
+++ M3U3 - Sitioweb/routes/index.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var router = express.Router();
+var nodemailer = require('nodemailer');
+
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/', async (req, res, next) => {
+  var nombre = req.body.nombre;
+  var apellido = req.body.apellido;
+  var email = req.body.email;
+  var tel = req.body.tel;
+  var mensaje = req.body.mensaje;
+
+
+
+  var obj = {
+    to: 'tpalermososa@gmail.com',
+    subject: 'CONTACTO WEB',
+    html: nombre + " se contacto a traves de la web y quiere mas informacion a este correo : " + email + ". <br> Ademas, hizo este comentario : " + mensaje + " <br> Su tel es : " + tel
+  }
+
+  var transport = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS
+    }
+  });
+
+
+
+  var info = await transport.sendMail(obj);
+
+  res.render('contacto', {
+    message: 'Mensaje enviado correctamente'
+  });
+
+});
+
+
+
+module.exports = router;
