7760806b56435e621cb0d26c2b6a77a3f5dd79e8
Talia Palermo Sosa <palermotaliabelen@gmail.com>
2022-06-18 23:31:38 -0300

    M5U03

---
+++ M5U03/routes/index.js
@@ -0,0 +1,74 @@
+var express = require('express');
+var router = express.Router();
+var nodemailer = require('nodemailer');
+var novedadesModel = require('../models/novedadesModel');
+var cloudinary = require ('cloudinary').v2;
+
+
+/* GET home page. */
+router.get('/', async function (req, res, next) {
+
+  var novedades = await novedadesModel.getNovedades ()
+
+  novedades = novedades.splice(0 , 5); // seleccionamos los primeros 5 elementos del array
+novedades = novedades.map(novedad => {
+if (novedad.img_id) {
+const imagen = cloudinary.url(novedad.img_id, {
+width: 460,
+crop: 'fill'
+});
+return {
+...novedad,
+imagen
+}
+} else {
+return {
+...novedad,
+imagen: '/images/noimage.jpg'
+}
+}
+});
+
+  res.render('index',{
+    novedades
+  });
+});
+
+
+router.post('/', async (req, res, next) => {
+  var nombre = req.body.nombre;
+  var apellido = req.body.apellido;
+  var email = req.body.email;
+  var tel = req.body.tel;
+  var mensaje = req.body.mensaje;
+
+
+
+  var obj = {
+    to: 'tpalermososa@gmail.com',
+    subject: 'CONTACTO WEB',
+    html: nombre + " se contacto a traves de la web y quiere mas informacion a este correo : " + email + ". <br> Ademas, hizo este comentario : " + mensaje + " <br> Su tel es : " + tel
+  }
+
+  var transport = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS
+    }
+  });
+
+
+
+  var info = await transport.sendMail(obj);
+
+  res.render('index', {
+    message: 'Mensaje enviado correctamente'
+  });
+
+});
+
+
+
+module.exports = router;
